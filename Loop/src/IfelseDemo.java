651e5dbf989f9ca86ca767075b1ebffec5407f5c
Helma <Helma@Lenovo-PC>
2022-05-31 14:09:18 +0530

    first commit

---
+++ Loop/src/IfelseDemo.java
@@ -0,0 +1,13 @@
+
+public class IfelseDemo {
+	public static void main(String[] args)
+	{
+		int a=15;
+		if (a>20)
+		System.out.println("a is greater then 10");
+		else
+			System.out.println("a is lesser than 10");
+		
+	}
+
+}
