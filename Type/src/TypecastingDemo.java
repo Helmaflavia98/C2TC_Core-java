04b5daaeb86e8244c3b0ec7e08d57eded122b643
Helma <Helma@Lenovo-PC>
2022-05-31 16:27:27 +0530

    first commit

---
+++ Type/src/TypecastingDemo.java
@@ -0,0 +1,11 @@
+
+public class TypecastingDemo {
+	public static void main (String[] args)
+	{
+		int a=10;
+		long l=a;
+		System.out.println(l);
+		
+	}
+
+}
---
+++ Type/src/TypecastingDemo2.java
@@ -0,0 +1,16 @@
+
+public class TypecastingDemo2 {
+	public static void main(String[] args)
+	{
+		int a=10;
+		long l=a;
+		double d=a;
+		
+		System.out.println(a);
+		System.out.println(l);
+		System.out.println(d);
+		
+		
+	}
+
+}
