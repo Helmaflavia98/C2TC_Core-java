e406b96a6be582450b28cb203e64912b8a2c798b
Helma <Helma@Lenovo-PC>
2022-08-22 00:39:29 +0530

    first commit

---
+++ CollectionFramework/src/HashSetdemo.java
@@ -0,0 +1,22 @@
+import java.util.TreeSet;
+import java.util.Iterator;
+import java.util.HashSet;
+
+public class HashSetdemo {
+	public static void main(String[] args) {
+		TreeSet<String> hs=new TreeSet<String>();
+        hs.add("Hash");
+        hs.add("Set");
+        hs.add("Demo");
+        hs.add("Demo");
+        //hs.add(new Integer(10));
+	   
+       Iterator i=hs.iterator();
+       while(i.hasNext())
+       {
+    	   Object o=i.next();
+    	   System.out.println(o);
+       }
+	}
+
+}
