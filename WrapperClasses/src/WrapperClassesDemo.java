9557ebf21c64efd65b7a3aeda1a60e2d57e3b4b0
Helma <Helma@Lenovo-PC>
2022-08-21 22:52:06 +0530

    first commit

---
+++ WrapperClasses/src/WrapperClassesDemo.java
@@ -0,0 +1,14 @@
+
+public class WrapperClassesDemo {
+	public static void main(String[] args) {
+		Integer i=new Integer(10);
+		System.out.println(i);
+		int b=i.intValue();
+		System.out.println(b);
+		int a=100;
+		Integer i1=a;
+		System.out.println(i1);
+		
+	}
+
+}
