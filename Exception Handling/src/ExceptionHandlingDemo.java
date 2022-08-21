8831f49c968394b12e7d5b7d061a9cf439883ac8
Helma <Helma@Lenovo-PC>
2022-08-21 23:05:35 +0530

    first commit

---
+++ Exception Handling/src/ExceptionHandlingDemo.java
@@ -0,0 +1,18 @@
+
+public class ExceptionHandlingDemo {
+	public static void main(String[] args) {
+		try {
+		int data=100/0;
+		}
+		catch(ArithmeticException ae)
+		{
+			ae.printStackTrace();
+		}
+		System.out.println("Exception Handling Demo");
+
+	}
+	
+	}
+
+
+
